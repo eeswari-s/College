e3b5f502b1aaf7cf41c679ef37ee02b8a9bf8c4c
eeswari-s <eeswarivaithee@gmail.com>
2025-08-04 14:49:24 +0530

    yes

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,8 @@ import connectDB from './Databases/Config.js';
 import authRoutes from "./Routers/AuthRoutes.js";
 import cors from 'cors';
 import protectedRoute from "./Routers/ProtectedRoute.js";
-
+import studentRouter from "./Routers/studentRouter.js";
+import companyRoutes from "./Routers/companyRouter.js";
 dotenv.config();
 const app = express();
 
@@ -15,6 +16,8 @@ const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes); 
 app.use("/api", protectedRoute); 
+app.use("/api/students", studentRouter);
+app.use("/api/company", companyRoutes);
 
 app.get("/", (req, res) => {
     res.send("🎓 Placement backend is running");
